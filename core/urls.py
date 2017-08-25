251a7d583faff258cf2b7b1ad7470c43f76c5887
Ashraful Islam <ashrafulrobin3@gmail.com>
2017-08-25 17:38:21 +0600

    Added fully featured registration view

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from core.views.profile_views import ProfileView, LoginView
+from core.views.profile_views import ProfileView, LoginView, RegistrationView
 
 urlpatterns = [
-    url(r'^login/$', LoginView.as_view(), name="api-login"),
+    url(r'^login/$', LoginView.as_view()),
+    url(r'^registration/$', RegistrationView.as_view()),
     url(r'^profiles/$', ProfileView.as_view(), name="profile-list-view"),
 ]
--- core/views/profile_views.py
+++ core/views/profile_views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth import authenticate
-from rest_framework import generics
+from django.contrib.auth.models import User
+from django.db import transaction
+from rest_framework import generics, status
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from core.models.profile import Profile
-from core.serializers.profile_serializers import *
+from core.serializers.profile_serializers import ProfileSerializer, LoginSerializer, RegistrationSerializer
 
 
 class ProfileView(generics.ListAPIView):
@@ -25,10 +27,7 @@ class LoginView(APIView):
             user = authenticate(username=username, password=password)
             try:
                 profile = Profile.objects.get(pk=user.pk)
-                try:
-                    token = Token.objects.get(user=user)
-                except Token.DoesNotExist:
-                    token = Token.objects.create(user=user)
+                token, _created = Token.objects.get_or_create(user_id=user.pk)
                 response = {
                     "token": token.key,
                     "user_role": profile.role.name
@@ -43,6 +42,24 @@ class RegistrationView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
 
     def post(self, request, *args, **kwargs):
-        user_reg_serializer = RegistrationSerializer(data=request.data)
-        if user_reg_serializer.is_valid(raise_exception=True):
-            pass
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            role = serializer.validated_data.get('role')
+            username = serializer.validated_data.get('username')
+            password = serializer.validated_data.get('password')
+            email = serializer.validated_data.get('email')
+            gender = serializer.validated_data.get('gender')
+
+            # Create user first
+            with transaction.atomic():
+                user = User.objects.create_user(username=username, email=email, password=password)
+                profile = Profile.objects.create(
+                    user_id=user.pk,
+                    role_id=role,
+                    gender=gender
+                )
+                if profile:
+                    data = serializer.validated_data
+                    data.pop('password')  # Trick to remove password from response
+                    return Response(data=data, status=status.HTTP_201_CREATED)
+        return Response(data=request.data, status=status.HTTP_400_BAD_REQUEST)
