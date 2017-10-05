1e4e8a864f6cec2ee27721f67757cf8d175ab584
Ashraful Islam <ashrafulrobin3@gmail.com>
2017-10-05 16:28:45 +0600

    Added details API for project and issue

--- pms/urls.py
+++ pms/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from pms.views.projects_views import ProjectView, IssueView
+from pms.views.projects_views import ProjectView, IssueView, ProjectDetailsView, IssueDetailsView
 
 urlpatterns = [
     url(r'^projects/$', ProjectView.as_view(), name="project-list-view"),
+    url(r'^projects/(?P<slug>[\w-]+)/$', ProjectDetailsView.as_view(), name="project-details-view"),
     url(r'^issues/$', IssueView.as_view(), name="issue-list-view"),
+    url(r'^issues/(?P<pk>[0-9]+)/$', IssueDetailsView.as_view(), name="issue-details-view"),
 ]
--- pms/views/projects_views.py
+++ pms/views/projects_views.py
@@ -1,4 +1,6 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from rest_framework.response import Response
 
 from pms.models.projects import Project, Issue
 from pms.serializers.projects_serializers import ProjectSerializer, IssueSerializer
@@ -11,6 +13,26 @@ class ProjectView(ListCreateAPIView):
     queryset = Project.objects.filter()
 
 
+class ProjectDetailsView(RetrieveUpdateAPIView):
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.filter()
+    lookup_field = 'slug'
+
+
 class IssueView(ListCreateAPIView):
+    serializer_class = IssueSerializer
+    queryset = Issue.objects.filter()
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            project = request.data.get('project')
+            assignee = request.data.get('assigned_to')
+            serializer.save(project_id=project, assigned_to_id=assignee)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data=request.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class IssueDetailsView(RetrieveUpdateAPIView):
     serializer_class = IssueSerializer
     queryset = Issue.objects.filter()
