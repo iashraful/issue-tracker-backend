4dc93754f00d6c3c74028860a7f4ec41d6ec3f7d
Ashraful Islam <ashraful@field.buzz>
2017-10-16 15:56:50 +0600

    running test from API (functionality is not working properly)

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
 from core.views.profile_views import ProfileView, LoginView, RegistrationView, LogoutView
+from core.views.tests_views import UnitTestAPIView
 from core.views.user_settings_view import UserConfirmView
 
 urlpatterns = [
@@ -9,4 +10,5 @@ urlpatterns = [
     url(r'^registration/$', RegistrationView.as_view()),
     url(r'^confirm-user/$', UserConfirmView.as_view()),
     url(r'^profiles/$', ProfileView.as_view(), name="profile-list-view"),
+    url(r'^run-tests/$', UnitTestAPIView.as_view(), name="run-test-api"),
 ]
