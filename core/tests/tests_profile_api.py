2e399ca9a21587afbb24c5cf1306b288a951fde3
Ashraful Islam <ashraful@field.buzz>
2017-10-16 12:41:58 +0600

    Writing unit tests

---
+++ core/tests/tests_profile_api.py
@@ -0,0 +1,12 @@
+from django.test.testcases import TestCase
+
+__author__ = 'Ashraful'
+
+
+class TestProfileApi(TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_profile_list(self):
+        print("Hello Test. I am writing unit tests. :)")
+        assert True
