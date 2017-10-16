ae8a76a36fbcb3d2dc4d1eb688bff6f3457a574b
Ashraful Islam <ashrafulrobin3@gmail.com>
2017-10-16 23:50:10 +0600

    Added due date saving issue

--- pms/views/issues_views.py
+++ pms/views/issues_views.py
@@ -1,7 +1,10 @@
+from datetime import datetime, timedelta
+
 from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 from rest_framework.response import Response
 
+from core.models.profile import Profile
 from pms.helpers.enums import ActionEnum
 from pms.models.activity_log import ActivityLog
 from pms.models.conversations import Conversation
@@ -21,7 +24,17 @@ class IssueView(ListCreateAPIView):
         if serializer.is_valid(raise_exception=True):
             project = request.data.get('project')
             assignee = request.data.get('assigned_to')
-            instance = serializer.save(author_id=profile, project_id=project, assigned_to_id=assignee)
+            due_date = request.data.get('due_date')
+            if due_date:
+                due_date = datetime.strptime(due_date, "%Y-%m-%d") + timedelta(days=1)
+            watchers = request.data.get('watchers')
+            instance = serializer.save(
+                author_id=profile, project_id=project, assigned_to_id=assignee, due_date=due_date
+            )
+            watchers_objects = list(Profile.objects.filter(pk__in=watchers))
+            if instance:
+                # Save watchers to issues
+                instance.watchers.add(*watchers_objects)
             try:
                 # Create activity log
                 ActivityLog.log(
