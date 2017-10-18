b06584655dbba0513e0f6a4b2c59e123da37f80b
Ashraful Islam <ashraful@field.buzz>
2017-10-18 18:29:08 +0600

    Fixed issue saving issue

--- pms/views/issues_views.py
+++ pms/views/issues_views.py
@@ -8,7 +8,7 @@ from core.models.profile import Profile
 from pms.helpers.enums import ActionEnum
 from pms.models.activity_log import ActivityLog
 from pms.models.conversations import Conversation
-from pms.models.projects import Issue
+from pms.models.projects import Issue, Project
 from pms.serializers.issues_serializers import IssueSerializer
 
 __author__ = 'Ashraful'
@@ -21,9 +21,20 @@ class IssueView(ListCreateAPIView):
     def post(self, request, *args, **kwargs):
         profile = request.user.profile.id
         serializer = self.serializer_class(data=request.data)
+        response_data = {}
         if serializer.is_valid(raise_exception=True):
             project = request.data.get('project')
             assignee = request.data.get('assigned_to')
+            try:
+                project = Project.objects.get(pk=project).pk
+            except (Project.DoesNotExist, AttributeError):
+                response_data['project'] = ['No matching Project found.']
+                return Response(data=response_data, status=status.HTTP_400_BAD_REQUEST)
+            try:
+                assignee = Profile.objects.get(pk=assignee).pk
+            except (Profile.DoesNotExist, AttributeError):
+                response_data['assigned_to'] = ['No matching User found.']
+                return Response(data=response_data, status=status.HTTP_400_BAD_REQUEST)
             due_date = request.data.get('due_date')
             if due_date:
                 due_date = datetime.strptime(due_date, "%Y-%m-%d") + timedelta(days=1)
@@ -33,10 +44,11 @@ class IssueView(ListCreateAPIView):
             instance = serializer.save(
                 author_id=profile, project_id=project, assigned_to_id=assignee, due_date=due_date
             )
-            watchers_objects = list(Profile.objects.filter(pk__in=watchers))
-            if instance:
-                # Save watchers to issues
-                instance.watchers.add(*watchers_objects)
+            if watchers:
+                watchers_objects = list(Profile.objects.filter(pk__in=watchers))
+                if instance:
+                    # Save watchers to issues
+                    instance.watchers.add(*watchers_objects)
             try:
                 # Create activity log
                 ActivityLog.log(
