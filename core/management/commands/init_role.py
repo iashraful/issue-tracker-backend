6d5b3fec06527ee459e1cc028d3c12370fd8d93c
Ashraful Islam <ashrafulrobin3@gmail.com>
2017-11-08 19:56:48 +0600

    Updated management command for creating role

--- core/management/commands/init_role.py
+++ core/management/commands/init_role.py
@@ -2,14 +2,14 @@ from django.core.management.base import BaseCommand
 from drf_role.models import Role
 from psycopg2._psycopg import IntegrityError
 
-ROLE_NAMES = ('Admin', 'Manager', 'Developer', 'Tester',)
+ROLE_NAMES = ('Manager', 'Developer', 'Tester',)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for _index, role_name in enumerate(ROLE_NAMES):
             try:
-                Role.objects.get_or_create(name=role_name, type=_index)
+                Role.objects.get_or_create(name=role_name, type=_index + 1)
             except IntegrityError:
                 continue
         print("Created roles {0}".format(ROLE_NAMES))
