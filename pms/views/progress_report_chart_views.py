14917fda030439b2dcdbd55d1e38945de13a7330
Ashraful Islam <ashrafulrobin3@gmail.com>
2017-11-14 22:44:07 +0600

    Updated the way of sending data. Need to be fixed for zero value when month in invisible

--- pms/views/progress_report_chart_views.py
+++ pms/views/progress_report_chart_views.py
@@ -16,12 +16,12 @@ class ProgressReportChartView(ListAPIView):
 
     def get(self, request, *args, **kwargs):
         chart_data = {
-            "line_chart": [],
+            "line_chart": {'data': [], "labels": []},
             "pie_chart": {},
         }
         now = datetime.now()
         project_wise_data = {}
-        for month_num in range(1, 12):
+        for month_num in range(1, 13):
             from_date = now.replace(month=month_num, day=1, minute=0, second=0)
             to_date = self.last_day_of_month(now.replace(month=month_num, minute=23, second=59))
             issues = Issue.objects.filter(
@@ -40,16 +40,20 @@ class ProgressReportChartView(ListAPIView):
                         issue.get('total_progress') // issue.get('issue_count')]
                 line_chart_data["label"] = issue.get('project__name')
                 line_chart_data["pk"] = issue.get('project_id')
-                if line_chart_data not in chart_data['line_chart']:
-                    chart_data['line_chart'].append(line_chart_data)
+                if line_chart_data not in chart_data['line_chart']['data']:
+                    chart_data['line_chart']['data'].append(line_chart_data)
                 # Assign the pie chart data
                 pie_chart_data["labels"].append(issue.get('project__name'))
+                # push the labels to chart data
+                this_month = now.replace(month=month_num).strftime("%B")
+                if this_month not in chart_data["line_chart"]["labels"]:
+                    chart_data["line_chart"]["labels"].append(this_month)
                 pie_chart_data["data"].append(issue.get('total_progress') / issue.get('issue_count'))
             chart_data['pie_chart'] = pie_chart_data
 
         for chart, chart_data_list in chart_data.items():
             if chart == "line_chart":
-                for data in chart_data_list:
+                for data in chart_data_list['data']:
                     if data.get('pk') in project_wise_data.keys():
                         # used mutability concept
                         data["data"] = project_wise_data[data.get('pk')]
