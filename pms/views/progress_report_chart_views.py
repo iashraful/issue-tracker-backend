c8434ddd56ab725c7eb52db6880f226f332bb19f
Ashraful Islam <ashrafulrobin3@gmail.com>
2017-11-16 20:55:05 +0600

    removed pie chart

--- pms/views/progress_report_chart_views.py
+++ pms/views/progress_report_chart_views.py
@@ -16,8 +16,7 @@ class ProgressReportChartView(ListAPIView):
 
     def get(self, request, *args, **kwargs):
         chart_data = {
-            "line_chart": {'data': [], "labels": []},
-            "pie_chart": {},
+            "line_chart": {'data': [], "labels": []}
         }
         now = datetime.now()
         project_wise_data = {}
@@ -29,7 +28,6 @@ class ProgressReportChartView(ListAPIView):
             ).values('project__name', 'project_id').annotate(
                 total_progress=Sum('progress'), issue_count=Count('pk')
             )
-            pie_chart_data = {"labels": [], "data": []}
             for issue in issues:
                 line_chart_data = {}
                 if issue.get('project_id') in project_wise_data.keys():
@@ -42,20 +40,15 @@ class ProgressReportChartView(ListAPIView):
                 line_chart_data["pk"] = issue.get('project_id')
                 if line_chart_data not in chart_data['line_chart']['data']:
                     chart_data['line_chart']['data'].append(line_chart_data)
-                # Assign the pie chart data
-                pie_chart_data["labels"].append(issue.get('project__name'))
                 # push the labels to chart data
                 this_month = now.replace(month=month_num).strftime("%B")
                 if this_month not in chart_data["line_chart"]["labels"]:
                     chart_data["line_chart"]["labels"].append(this_month)
-                pie_chart_data["data"].append(issue.get('total_progress') / issue.get('issue_count'))
-            chart_data['pie_chart'] = pie_chart_data
 
         for chart, chart_data_list in chart_data.items():
-            if chart == "line_chart":
-                for data in chart_data_list['data']:
-                    if data.get('pk') in project_wise_data.keys():
-                        # used mutability concept
-                        data["data"] = project_wise_data[data.get('pk')]
+            for data in chart_data_list['data']:
+                if data.get('pk') in project_wise_data.keys():
+                    # used mutability concept
+                    data["data"] = project_wise_data[data.get('pk')]
 
         return Response(data=chart_data, status=status.HTTP_200_OK)
