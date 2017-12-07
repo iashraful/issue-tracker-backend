92908d95fb3a17fb25084d41ea8f59b06ad8f1c0
Ashraful Islam <ashraful@field.buzz>
2017-12-07 15:49:25 +0600

    Updated daily report data

--- pms/views/daily_reports_views.py
+++ pms/views/daily_reports_views.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 from datetime import datetime
 
-from django.db.models import Count
+from django.db.models import Count, Sum
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -15,25 +15,29 @@ class DailyReportsView(APIView):
         data['user_wise'] = self.get_user_wise_data()
         return Response(data=data)
 
-    def get_project_wise_data(self):
+    def get_user_wise_data(self):
         today = datetime.now()
         time_from = today.replace(hour=0, minute=0, second=0)
         time_to = today.replace(hour=23, minute=59, second=59)
 
         base_queryset = Issue.objects.filter(updated_at__gte=time_from, updated_at__lte=time_to)
         report_data = list()
-        plain_data = base_queryset.values('project_id', 'project__name', today_updated=Count('pk'))
+        plain_data = base_queryset.values(
+            'updated_by_id', 'updated_by__user__username', 'project__slug', 'project__name',
+            today_updated=Count('pk'), total_progress=Sum('progress')
+        )
         for d in plain_data:
+            project = Project.objects.filter(slug=d.get('project__slug')).first()
             data = OrderedDict()
-            project = Project.objects.get(pk=d.get('project_id'))
-            data['project'] = project.name
-            data['total_issues'] = project.issue_set.count()
-            data['today_updated'] = d.get('today_updated')
-            data['today_created'] = project.issue_set.filter(created_at__gte=time_from, created_at__lte=time_to).count()
+            data['updated_by_id'] = d.get('updated_by_id')
+            data['updated_by_name'] = d.get('updated_by__user__username')
+            data['project_slug'] = d.get('project__slug')
+            data['project_name'] = d.get('project__name')
+            data['total_progress'] = int(d.get('total_progress') / project.issue_set.count())
             report_data.append(data)
         return report_data
 
-    def get_user_wise_data(self):
+    def get_project_wise_data(self):
         today = datetime.now()
         time_from = today.replace(hour=0, minute=0, second=0)
         time_to = today.replace(hour=23, minute=59, second=59)
